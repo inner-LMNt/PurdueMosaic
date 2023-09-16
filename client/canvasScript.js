9c436379f77748c8f79d7aab38b33859136620bf
Alex Liu <alex369888@gmail.com>
2023-09-16 19:20:27 -0400

    enable sync

--- client/canvasScript.js
+++ client/canvasScript.js
@@ -1,183 +1,211 @@
 // script.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("before grid creation");
+    var socket = io();
 
-let currentColor = "#000000"; // Default color
-
-// Function to create a single pixel element
-function createPixel() {
-    const pixel = document.createElement("div");
-    pixel.className = "pixel";
-
-    // Add click event to change the pixel color
-    pixel.addEventListener("click", () => {
-        const newColor = getColor(); // Get the selected color
-        pixel.style.backgroundColor = newColor; // Set the selected color
+    // Listen for the 'initialPixels' event to get the initial pixel data from the server
+    socket.on('initialPixels', (initialPixels) => {
+      // Update the pixel colors based on the initial data from the server
+      const pixels = document.querySelectorAll('.pixel');
+      pixels.forEach((pixel, index) => {
+        pixel.style.backgroundColor = initialPixels[index];
+      });
     });
+  
+    // Listen for the 'updatePixel' event to receive real-time updates from other clients
+    socket.on('updatePixel', (data) => {
+      // Update the pixel color in real-time
+      const pixels = document.querySelectorAll('.pixel');
+      pixels[data.index].style.backgroundColor = data.color;
+    });
+  
+    let currentColor = '#000000'; // Default color
+  
+    // Function to create a single pixel element
+    function createPixel() {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+  
+      // Add click event to change the pixel color
+      pixel.addEventListener('click', () => {
+        const color = currentColor;
+        const index = Array.from(pixel.parentNode.children).indexOf(pixel);
+        pixel.style.backgroundColor = color; // Set the selected color
+  
+        // Send the pixel update to the server
+        socket.emit('updatePixel', { index, color });
+        console.log('Emitted initial pixels to a user');
+      });
+  
+      return pixel;
+    }
 
-    return pixel;
-}
+    // Function to set the current color
+    function setCurrentColor(color) {
+        currentColor = color;
+        const colorPresets = document.querySelectorAll(".color-preset");
 
-// Function to set the current color
-function setCurrentColor(color) {
-    currentColor = color;
+        // Remove the "selected" class from all presets and reset border colors
+        colorPresets.forEach(preset => {
+            preset.classList.remove("selected");
+            preset.style.borderColor = "black"; // Reset border color to black for all presets
+        });
+
+        // Find the selected preset by data-color attribute and add the "selected" class
+        const selectedPreset = document.querySelector(`[data-color="${color}"]`);
+        if (selectedPreset) {
+            selectedPreset.classList.add("selected");
+            selectedPreset.style.borderColor = darkenColor(color); // Apply darker border color
+        }
+
+        // Update the custom color button to reflect the selected color
+        const customColorButton = document.getElementById("color-picker");
+        customColorButton.value = color;
+    }
+
+    // Function to darken a given color
+    function darkenColor(color) {
+        // Parse the color into its RGB components
+        const r = parseInt(color.slice(1, 3), 16);
+        const g = parseInt(color.slice(3, 5), 16);
+        const b = parseInt(color.slice(5, 7), 16);
+
+        // Darken the color by reducing each component by 40%
+        const darkenedR = Math.max(0, r - Math.round(r * 0.4));
+        const darkenedG = Math.max(0, g - Math.round(g * 0.4));
+        const darkenedB = Math.max(0, b - Math.round(b * 0.4));
+
+        // Convert the darkened components back to a HEX color
+        return `#${darkenedR.toString(16).padStart(2, "0")}${darkenedG.toString(16).padStart(2, "0")}${darkenedB.toString(16).padStart(2, "0")}`;
+    }
+
+    // Function to get the selected color from the color wheel
+    function getColor() {
+        const colorPicker = document.getElementById("color-picker");
+        return colorPicker.value;
+    }
+
+    function createGrid() {
+        console.log('Creating grid...');
+        const canvasContainer = document.querySelector(".canvas-container");
+
+        for (let i = 0; i < 30 * 50; i++) {
+            const pixel = createPixel();
+            pixel.style.backgroundColor = "fff"; // Set the initial background color to white
+            pixel.style.width = "20px"; // Set the pixel size
+            pixel.style.height = "20px"; // Set the pixel size
+            canvasContainer.appendChild(pixel);
+        }
+        console.log("grid created")
+    }
+
+
+    // Call the function to create the grid when the page loads
+    window.addEventListener("load", createGrid);
+
+    // Event listeners for preset color circles
     const colorPresets = document.querySelectorAll(".color-preset");
 
-    // Remove the "selected" class from all presets and reset border colors
     colorPresets.forEach(preset => {
-        preset.classList.remove("selected");
-        preset.style.borderColor = "black"; // Reset border color to black for all presets
+        const color = preset.getAttribute("data-color");
+        
+        // Add click event to set the current color when a preset is clicked
+        preset.addEventListener("click", () => {
+            setCurrentColor(color);
+        });
+
+        // Initialize the current color based on the default color
+        if (color === currentColor) {
+            preset.classList.add("selected");
+        }
     });
 
-    // Find the selected preset by data-color attribute and add the "selected" class
-    const selectedPreset = document.querySelector(`[data-color="${color}"]`);
-    if (selectedPreset) {
-        selectedPreset.classList.add("selected");
-        selectedPreset.style.borderColor = darkenColor(color); // Apply darker border color
-    }
-
-    // Update the custom color button to reflect the selected color
-    const customColorButton = document.getElementById("color-picker");
-    customColorButton.value = color;
-}
-
-// Function to darken a given color
-function darkenColor(color) {
-    // Parse the color into its RGB components
-    const r = parseInt(color.slice(1, 3), 16);
-    const g = parseInt(color.slice(3, 5), 16);
-    const b = parseInt(color.slice(5, 7), 16);
-
-    // Darken the color by reducing each component by 40%
-    const darkenedR = Math.max(0, r - Math.round(r * 0.4));
-    const darkenedG = Math.max(0, g - Math.round(g * 0.4));
-    const darkenedB = Math.max(0, b - Math.round(b * 0.4));
-
-    // Convert the darkened components back to a HEX color
-    return `#${darkenedR.toString(16).padStart(2, "0")}${darkenedG.toString(16).padStart(2, "0")}${darkenedB.toString(16).padStart(2, "0")}`;
-}
-
-// Function to get the selected color from the color wheel
-function getColor() {
+    // Initialize the color picker to the default color
     const colorPicker = document.getElementById("color-picker");
-    return colorPicker.value;
-}
-
-function createGrid() {
-    const canvasContainer = document.querySelector(".canvas-container");
-
-    for (let i = 0; i < 30 * 50; i++) {
-        const pixel = createPixel();
-        pixel.style.backgroundColor = "#fff"; // Set the initial background color to white
-        pixel.style.width = "20px"; // Set the pixel size
-        pixel.style.height = "20px"; // Set the pixel size
-        canvasContainer.appendChild(pixel);
-    }
-}
-
-
-// Call the function to create the grid when the page loads
-window.addEventListener("load", createGrid);
-
-// Event listeners for preset color circles
-const colorPresets = document.querySelectorAll(".color-preset");
-
-colorPresets.forEach(preset => {
-    const color = preset.getAttribute("data-color");
-    
-    // Add click event to set the current color when a preset is clicked
-    preset.addEventListener("click", () => {
+    colorPicker.value = currentColor;
+    colorPicker.addEventListener("input", () => {
+        const color = getColor();
         setCurrentColor(color);
     });
 
-    // Initialize the current color based on the default color
-    if (color === currentColor) {
-        preset.classList.add("selected");
+    // Function to capture canvas content and trigger download
+    function downloadCanvas() {
+
+        // Create a temporary canvas with the content of the grid
+        const tempCanvas = createTempCanvas();
+        
+        if (tempCanvas === null) {
+            console.error('Failed to create a temporary canvas.'); // Log an error if the temporary canvas creation failed
+            return;
+        }
+
+        const dataURL = tempCanvas.toDataURL('image/png');
+
+        // Create a download link
+        const downloadLink = document.createElement('a');
+        downloadLink.href = dataURL;
+        downloadLink.download = 'canvas.png'; // Set the default download filename
+
+        // Trigger a click event on the download link to start the download
+        downloadLink.click();
     }
+
+    function createTempCanvas() {
+        const canvasContainer = document.querySelector('.canvas-container');
+        const tempCanvas = document.createElement('canvas');
+        tempCanvas.width = 50 * 20; // Width of the canvas in pixels
+        tempCanvas.height = 30 * 20; // Height of the canvas in pixels
+        const context = tempCanvas.getContext('2d');
+
+        const pixels = canvasContainer.querySelectorAll('.pixel');
+
+        if (pixels.length === 0) {
+            console.error('No pixels found in the grid.');
+            return null;
+        }
+
+        // Loop through all the pixels and draw them onto the temporary canvas
+        pixels.forEach((pixel, index) => {
+            const x = (index % 50) * 20; // Adjust for pixel size
+            const y = Math.floor(index / 50) * 20; // Adjust for pixel size
+
+            // Retrieve the pixel's background color
+            const pixelColor = window.getComputedStyle(pixel).backgroundColor;
+
+            // Set the fill color to the pixel's background color
+            context.fillStyle = pixelColor;
+
+            // Draw a rectangle representing the pixel
+            context.fillRect(x, y, 20, 20); // Adjust for pixel size
+        });
+
+        return tempCanvas;
+    }
+
+    // Function to export the temporary canvas as a PNG image
+    function exportCanvasAsPNG() {
+        const tempCanvas = createTempCanvas();
+        const dataURL = tempCanvas.toDataURL('image/png');
+
+        // Create a download link
+        const downloadLink = document.createElement('a');
+        downloadLink.href = dataURL;
+        downloadLink.download = 'canvas.png'; // Set the default download filename
+
+        // Trigger a click event on the download link to start the download
+        downloadLink.click();
+    }
+
+
+    function clearCanvas() { //will clear canvas when called
+        const pixels = document.querySelectorAll('.pixel');
+                pixels.forEach(pixel => {
+                    pixel.style.backgroundColor = "#fff"; // clears pixel background color
+                });
+    }
+
+    // Add a click event listener to the download button
+    const downloadButton = document.getElementById('download-button');
+    downloadButton.addEventListener('click', downloadCanvas);
+
 });
-
-// Initialize the color picker to the default color
-const colorPicker = document.getElementById("color-picker");
-colorPicker.value = currentColor;
-colorPicker.addEventListener("input", () => {
-    const color = getColor();
-    setCurrentColor(color);
-});
-
-// Function to capture canvas content and trigger download
-function downloadCanvas() {
-
-    // Create a temporary canvas with the content of the grid
-    const tempCanvas = createTempCanvas();
-    
-    if (tempCanvas === null) {
-        console.error('Failed to create a temporary canvas.'); // Log an error if the temporary canvas creation failed
-        return;
-    }
-
-    const dataURL = tempCanvas.toDataURL('image/png');
-
-    // Create a download link
-    const downloadLink = document.createElement('a');
-    downloadLink.href = dataURL;
-    downloadLink.download = 'canvas.png'; // Set the default download filename
-
-    // Trigger a click event on the download link to start the download
-    downloadLink.click();
-}
-
-function createTempCanvas() {
-    const canvasContainer = document.querySelector('.canvas-container');
-    const tempCanvas = document.createElement('canvas');
-    tempCanvas.width = 50 * 20; // Width of the canvas in pixels
-    tempCanvas.height = 30 * 20; // Height of the canvas in pixels
-    const context = tempCanvas.getContext('2d');
-
-    const pixels = canvasContainer.querySelectorAll('.pixel');
-
-    if (pixels.length === 0) {
-        console.error('No pixels found in the grid.');
-        return null;
-    }
-
-    // Loop through all the pixels and draw them onto the temporary canvas
-    pixels.forEach((pixel, index) => {
-        const x = (index % 50) * 20; // Adjust for pixel size
-        const y = Math.floor(index / 50) * 20; // Adjust for pixel size
-
-        // Retrieve the pixel's background color
-        const pixelColor = window.getComputedStyle(pixel).backgroundColor;
-
-        // Set the fill color to the pixel's background color
-        context.fillStyle = pixelColor;
-
-        // Draw a rectangle representing the pixel
-        context.fillRect(x, y, 20, 20); // Adjust for pixel size
-    });
-
-    return tempCanvas;
-}
-
-// Function to export the temporary canvas as a PNG image
-function exportCanvasAsPNG() {
-    const tempCanvas = createTempCanvas();
-    const dataURL = tempCanvas.toDataURL('image/png');
-
-    // Create a download link
-    const downloadLink = document.createElement('a');
-    downloadLink.href = dataURL;
-    downloadLink.download = 'canvas.png'; // Set the default download filename
-
-    // Trigger a click event on the download link to start the download
-    downloadLink.click();
-}
-
-
-function clearCanvas() { //will clear canvas when called
-    const pixels = document.querySelectorAll('.pixel');
-            pixels.forEach(pixel => {
-                pixel.style.backgroundColor = "#fff"; // clears pixel background color
-            });
-}
-
-// Add a click event listener to the download button
-const downloadButton = document.getElementById('download-button');
-downloadButton.addEventListener('click', downloadCanvas);
--- server/index.js
+++ server/index.js
@@ -1,25 +1,44 @@
 const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io')
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server)
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(express.static('../client'))
+// Serve static files
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
+const pixels = new Array(30 * 50).fill('#ffffff'); // Initialize pixels with default color
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
+  console.log('A user connected');
+
+  // Send the initial pixel data to the client
+  socket.emit('initialPixels', pixels);
+
+  // Handle messages from clients to update pixels
+  socket.on('updatePixel', (data) => {
+    // Update the pixel color
+    pixels[data.index] = data.color;
+
+    // Broadcast the updated pixel to all connected clients
+    io.emit('updatePixel', { index: data.index, color: data.color });
   });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  // Handle disconnection
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
